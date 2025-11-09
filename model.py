e3d7e04f09b1bd2285df8cc8d16166d24a3e0d2c
RAHUL-MOHANTY <mohantyrahul257@gmail.com>
2025-11-09 18:28:50 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import pickle
 # Import necessary libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 # Load the dataset
